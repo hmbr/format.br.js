cdf7cf8adca6f8e44cdb4333d725c9960b0d3c6e
helder <helderribas@gmail.com>
2011-04-15 18:49:26 -0300

    arrumando o cnpj

--- public/javascripts/format.br.js
+++ public/javascripts/format.br.js
@@ -1,7 +1,3 @@
-/**
- * removeSpaces
- * @param string */
-
 function removePontuaction(string) {
 	var _string = string;
 	return _string.replace(/[-. ]/g,"");
@@ -20,9 +16,30 @@ function removeAllButNumbers(string,decimal) {
 	return string.replace(re,"");
 }
 
-/**
- * formatarDecimal
- * @param arg1 */
+function formatCnpj(string) {
+	_string  = removeAllButNumbers(string,'');
+	var _array = (_string + '').split('');
+
+	var count = 0;
+
+	var result = '';
+	var sep = '';
+	while(_array.length >0 ){
+		count ++;
+		sep = '';
+		if ( count == 3){
+			sep = '-';
+		}else if ( count == 7){
+			sep = '/';
+		}else if ( count == 10 || count == 13 ){
+			sep = '.';
+		}
+
+		result = _array.pop() + sep + result;
+	}
+	return result;
+
+}
 
 function format(number,options) {
 
@@ -33,7 +50,7 @@ function format(number,options) {
 	var integer;
 
 	var position = _number.lastIndexOf(decimal_char);
-	
+
 	if (position >-1){
 		decimal = _number.substr(position +1);
 		integer = Number(_number.substr(0,position));
@@ -41,22 +58,21 @@ function format(number,options) {
 		decimal = '';
 		integer = Number(_number);
 	}
-	
+
 	var times = options.fractional+1 - decimal.length;
 
 	if (times <0){
 		times = 0;
 	}
-	
+
 	var completation = (new Array(times).join('0'));	
 	decimal  = (decimal + completation);
 
-	
 	var _integer = '';
 	var _array = (integer + '').split('');
-	
+
 	var count = 0;
-	
+
 	while(_array.length >0 ){
 		count ++;
 		if (count == 4){
@@ -71,18 +87,25 @@ function format(number,options) {
 }
 
 (function(jQuery) {
-    
-    $.fn.formatNumber = function(options){
-        var _options = {decimal:',', grouping:'.',fractional:6};
-        if (options){
-            _options = options;
-        }
-        
-        var number = $(this).val();
-        if (number){
-            var result = format(number,_options);
-            $(this).val(result);
-        }
-    };
-          
- })(jQuery);
+
+	$.fn.formatNumber = function(options){
+		var _options = {decimal:',', grouping:'.',fractional:6};
+		if (options){
+			_options = options;
+		}
+
+		var number = $(this).val();
+		if (number){
+			var result = format(number,_options);
+			$(this).val(result);
+		}
+	};
+
+	$.fn.formatCnpj = function(options){
+		var number = $(this).val();
+		if (number){
+			var result = formatCnpj(number);
+			$(this).val(result);
+		}
+	};    
+})(jQuery);
--- spec/javascripts/format_br_spec.js
+++ spec/javascripts/format_br_spec.js
@@ -1,46 +1,82 @@
 describe('FormatBR', function () {
-   it('remover pontuacao', function () {    
-    expect(removePontuaction('')).toEqual('');
-    expect(removePontuaction('220.105.688-93')).toEqual('22010568893');
-    expect(removePontuaction('11 11')).toEqual('1111');
-   });
-   
-   it('remover', function () {    
-    expect(removeAllButNumbers('')).toEqual('');
-    expect(removeAllButNumbers('100,000',',')).toEqual('100,000');
-    expect(removeAllButNumbers('11.11')).toEqual('1111');
-   });
-   
-   it('formatar numeros', function () {
-      expect(format('100,000',{decimal:',', grouping:'.',fractional:3})).toEqual('100,000');
-      expect(format('100,010',{decimal:',', grouping:'.',fractional:3})).toEqual('100,010');
-      expect(format('100',{decimal:',', grouping:'.',fractional:3})).toEqual('100,000');
-   });
-   
-   it('formatar numeros', function () {
-   
-      expect(format('1234',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234,000');
-      expect(format('1234567',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,000');
-      expect(format('1234567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
-      expect(format('1.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
-      expect(format('01.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
-      expect(format('101.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('101.234.567,890');
-      expect(format('101.234.567,8091',{decimal:',', grouping:'.',fractional:3})).toEqual('101.234.567,809');
 
-   });
-   
-   it('testar formulario', function () {
-      loadFixtures("format_form.html");
-      $("#numero").val("1234");
-      $("#numero").formatNumber();
-      expect($("#numero")).toHaveValue("1.234,000000");
-       });
-   
-   
-    it('testar campo vazio formulario', function () {
-      loadFixtures("format_form.html");
-      $("#numero").val("");
-      $("#numero").formatNumber();
-      expect($("#numero")).toHaveValue("");
-       });
+	it('remover pontuacao', function () {    
+		expect(removePontuaction('')).toEqual('');
+		expect(removePontuaction('220.105.688-93')).toEqual('22010568893');
+		expect(removePontuaction('11 11')).toEqual('1111');
+	});
+
+	it('remover', function () {    
+		expect(removeAllButNumbers('')).toEqual('');
+		expect(removeAllButNumbers('100,000',',')).toEqual('100,000');
+		expect(removeAllButNumbers('11.11')).toEqual('1111');
+	});
+
+	it('formatar numeros', function () {
+		expect(format('100,000',{decimal:',', grouping:'.',fractional:3})).toEqual('100,000');
+		expect(format('100,010',{decimal:',', grouping:'.',fractional:3})).toEqual('100,010');
+		expect(format('100',{decimal:',', grouping:'.',fractional:3})).toEqual('100,000');
+	});
+
+	it('formatar cnpj', function () {
+		expect(formatCnpj('75101873000270')).toEqual('75.101.873/0002-70');
+	});
+
+
+	it('formatar cnpj', function () {
+		expect(formatCnpj('002.558.134/0001-58')).toEqual('002.558.134/0001-58');
+	});
+
+	it('formatar cnpj', function () {
+		expect(formatCnpj('002558134000158')).toEqual('002.558.134/0001-58');
+	});
+
+	it('formatar cnpj', function () {
+		expect(formatCnpj('00255813400015')).toEqual('00.255.813/4000-15');
+	});
+
+	it('formatar numeros', function () {
+		expect(format('1234',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234,000');
+		expect(format('1234567',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,000');
+		expect(format('1234567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
+		expect(format('1.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
+		expect(format('01.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('1.234.567,890');
+		expect(format('101.234.567,89',{decimal:',', grouping:'.',fractional:3})).toEqual('101.234.567,890');
+		expect(format('101.234.567,8091',{decimal:',', grouping:'.',fractional:3})).toEqual('101.234.567,809');
+	});
+
+	it('testar formulario', function () {
+		loadFixtures("format_form.html");
+		$("#numero").val("1234");
+		$("#numero").formatNumber();
+		expect($("#numero")).toHaveValue("1.234,000000");
+	});
+
+	it('testar campo vazio formulario', function () {
+		loadFixtures("format_form.html");
+		$("#numero").val("");
+		$("#numero").formatNumber();
+		expect($("#numero")).toHaveValue("");
+	});
+
+	it('testar formulario - CNPJ', function () {
+		loadFixtures("format_form.html");
+		$("#numero").val("002.558.134/0001-58");
+		$("#numero").formatCnpj();
+		expect($("#numero")).toHaveValue("002.558.134/0001-58");
+	});
+
+	it('testar formulario - CNPJ', function () {
+		loadFixtures("format_form.html");
+		$("#numero").val("002558134/0001-58");
+		$("#numero").formatCnpj();
+		expect($("#numero")).toHaveValue("002.558.134/0001-58");
+	});
+
+	it('testar formulario - CNPJ', function () {
+		loadFixtures("format_form.html");
+		$("#numero").val("002558134/000-58");
+		$("#numero").formatCnpj();
+		expect($("#numero")).toHaveValue("00.255.813/4000-58");
+	});
 });
